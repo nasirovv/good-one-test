bf87a8853c6a48a581976c06096135a88e5e0972
nasirovv <nasirov0017@gmail.com>
2021-10-26 14:48:18 +0500

    api for calculate updated

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Material;
+use App\Models\Product;
 use App\Models\Warehouse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -12,15 +13,23 @@ class CalculateController extends Controller
 {
     public function index(Request $request)
     {
-        $data = [];
-        $materials = DB::table('material_product')
-            ->where('product_id', $request->get('product_id'))
-            ->select('material_id', 'quantity')
-            ->get();
-        foreach ($materials as $material){
-            $data[] = $this->test($material->material_id, $material->quantity);
+        $result = [];
+        foreach($request->get('products') as $product){
+            $data = [
+                'product_name' => Product::query()->find($product['product_id'])->name,
+                'product_quantity' => $product['amount']
+            ];
+            $materials = DB::table('material_product')
+                ->where('product_id', $product['product_id'])
+                ->select('material_id', 'quantity')
+                ->get();
+            foreach ($materials as $material){
+                $data['product_materials'][] = $this->test($material->material_id, $material->quantity * $product['amount']);
+            }
+            $result[] = $data;
         }
-        return $data;
+
+        return $result;
     }
 
     public function calculate($material_id, $quantity){
@@ -37,18 +46,27 @@ class CalculateController extends Controller
         $materials = Warehouse::query()
             ->where('material_id', $material_id)
             ->get();
+        $getter = $quantity;
         foreach ($materials as $warehouse){
                 $skip = 0;
-                $get = 0;
                 $house_id = $warehouse->id;
-                if($warehouse->amount >= $quantity ){
-                    $get = $quantity;
-                    $array += [
+                if($warehouse->amount >= $getter && $getter){
+                    $array[] = [
+                        'warehouse_id' => $house_id,
+                        'material_name' => $warehouse->material_id,
+                        'quantity' => $getter,
+                        'price' => $warehouse->price
+                    ];
+                    $getter = 0;
+                }else {
+                    $get = $warehouse->amount;
+                    $array[] = [
                         'warehouse_id' => $house_id,
                         'material_name' => $warehouse->material_id,
                         'quantity' => $get,
                         'price' => $warehouse->price
                     ];
+                    $getter -= $get;
                 }
             };
         return $array;
